89146e0995a1f6ad53c91c3e3e55a8d91144939d
faozybg <segunfaozan@gmail.com>
2023-03-24 10:06:07 -0700

    third commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HeaderSplash from "./components/HeaderSplash/HeaderSplash";
+
+const App = () => {
+  return (
+    <div className="App">
+      <HeaderSplash />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HeaderSplash/HeaderSplash.jsx
@@ -0,0 +1,7 @@
+import "./headerSplash.scss";
+
+const HeaderSplash = () => {
+  return <header className="header">Some Text...</header>;
+};
+
+export default HeaderSplash;
