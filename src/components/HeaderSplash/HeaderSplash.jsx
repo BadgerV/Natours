07ab6107c2211feeaacfb7a593849da8ec00905b
faozybg <segunfaozan@gmail.com>
2023-03-25 11:34:16 -0700

    added button

--- src/components/HeaderSplash/HeaderSplash.jsx
+++ src/components/HeaderSplash/HeaderSplash.jsx
@@ -1,7 +1,24 @@
 import "./headerSplash.scss";
 
 const HeaderSplash = () => {
-  return <header className="header">Some Text...</header>;
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img src="/assets/logo-white.png" alt="Logo" class="logo" />
+      </div>
+
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub">is where life happens</span>
+        </h1>
+
+        <a href="#" className="btn btn-white button-animated">
+          Discover our tours
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default HeaderSplash;
